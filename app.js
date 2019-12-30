4b4eaf81e6326123c292ac3ab70374d5a5dba96b
Andy <xcessivepupil2@gmail.com>
2019-12-30 13:22:48 -0800

    implement React components

--- app.js
+++ app.js
@@ -1,1 +1,46 @@
 // TODO
+var items = ['Kale','Cucumbers','Bananas','Apples'];
+//var GroceryListItem = (prop) => (<li>{prop.groceryItem}</li>);
+
+class GroceryListItem extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      done: false
+    };
+  }
+
+  onListItemHover() {
+    this.setState({
+      done: !this.state.done
+    });
+  }
+
+  render() {
+    var style = {
+      fontWeight: this.state.done ? 'bold' : 'normal'
+    };
+
+    return (
+      <li style={style} onMouseOver={this.onListItemHover.bind(this)}>{this.props.item}</li>
+    );
+  }
+}
+
+var GroceryList = (props) => (
+  <ul>
+    {props.groceryItems.map(item =>
+      <GroceryListItem item={item} />
+    )}
+  </ul>
+);
+
+var App = () => (
+  <div>
+    <h2>My Grocery List</h2>
+    <GroceryList groceryItems={items}/>
+  </div>
+);
+
+ReactDOM.render(<App />, document.getElementById("app"));
+
